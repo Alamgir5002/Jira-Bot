c8d72dcba7ab17fec41a512e1afbf429e7596711
Alamgir Hassan <alamgir.hassan@uworx.co.uk>
2024-03-10 21:28:27 +0500

    Thrown and catched exception if source details not found

--- Jira-bot/Services/SourceWorklogService.cs
+++ Jira-bot/Services/SourceWorklogService.cs
@@ -54,6 +54,7 @@ namespace Jira_bot.Services
             {
                 logger.LogError($"Exception occurred: {exception.Message}");
                 await turnContext.SendActivityAsync(MessageFactory.Text(exception.Message, exception.Message), cancellationToken);
+                await ShowAddSourceCredentialsCard(turnContext, cancellationToken);
             }
         }
 
